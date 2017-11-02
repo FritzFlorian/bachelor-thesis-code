1ed53d071efaf2e25709920a808391470e2bb66a
FritzFlorian <flo.fritz@t-online.de>
2017-11-02 19:32:07 +0100

    Add BasicClient to networking code
    The BasicClient acts as a base for building an actual reversi client.
    This functionality is kept seperate from game code to be able to reuse it later for different client types to collect game data.
    
    Note: Code is still untested until BasicServer is also implemented to test them against each other.

--- reversi/network_core.py
+++ reversi/network_core.py
@@ -1,8 +1,146 @@
 """Network Code for ReversiXT.
 
 This includes package encoding/decoding and all primitives,
-but excludes actual logic for a full client/server."""
+but excludes actual logic for a full client/server.
+
+The BasicClient and BasicServer classes wrap all functions needed
+to run a client/server, but do not include code for server specific control flow.
+They should be used to build up the actual client/server as well as
+altered implementations useful for gathering data."""
 from reversi.game_core import Board, Field, PLAYERS
+import socket
+import logging
+
+
+DEFAULT_PORT = 7777
+DEFAULT_HOST = 'localhost'
+# Always timeout moves if they take longer than 5 Minutes, no matter the depth limit
+MOVE_TIMEOUT = 5 * 60
+# Clients must answer basic request (not moves) in at most 10 seconds
+GENERAL_TIMEOUT = 10
+
+
+class DisqualifiedError(Exception):
+    """All 'known' client errors and timeouts lead to an disqualified exception for that player."""
+    def __init__(self, group, player, message, cause=None):
+        self.group = group
+        self.player = player
+        self.message = message
+        self.cause = cause
+
+
+class BasicClient:
+    """Basic ReversiXT Network Client. Use this to build your custom Client.
+
+    Basic Usage:
+    >>> client = BasicClient(4242)
+    >>> client.start()
+    >>>
+    >>> one = client.accept_client()
+    >>> two = client.accept_client()
+    >>>
+    >>> client.set_player_for_group(one, Field.PLAYER_ONE)
+    >>> client.set_player_for_group(two, Field.PLAYER_TWO)
+    >>>
+    >>> client.send_player_message(Field.PLAYER_ONE, MoveRequestMessage(...))
+    >>> response = client.read_player_message(Field.PLAYER_ONE, MoveResponseMessage, 10)
+    >>>
+    >>> for p in [Field.PLAYER_ONE, Field.PLAYER_TWO]:
+    >>>     client.send_player_message(p, MoveNotificationMessage(...))
+    >>>
+    >>> client.stop()
+    """
+    def __init__(self, port=DEFAULT_PORT):
+        self.logger = logging.getLogger("BasicClient ({})".format(port))
+
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.clients_by_group = dict()
+        self.clients_by_player = dict()
+
+        self.player_by_client = dict()
+        self.group_by_client = dict()
+
+        self.next_group = 0
+
+    def start(self):
+        self.logger.info("Starting server on port {}...".format(self.port))
+        self.server.bind((socket.gethostname(), self.port))
+
+    def stop(self):
+        self.server.close()
+
+    def accept_client(self):
+        """Waits for a client to connect and send its group number. Returns the group number."""
+        self.logger.info("Waiting for client connection...")
+        client, address = self.server.accept()
+
+        # Read the group, simply assign one if there are conflicts
+        group = self._read_message(client, GroupNumberMessage).group_number
+        while group in self.clients_by_group:
+            group = self.next_group
+            self.next_group = self.next_group + 1
+
+        self.clients_by_group[group] = client
+        self.group_by_client[client] = group
+
+        self.logger.info("Client from group {} connected ({}).".format(group, address))
+        return group
+
+    def set_player_for_group(self, group, player):
+        client = self.clients_by_group[group]
+        self._send_message(client, PlayerNumberMessage(player))
+
+        self.clients_by_player[player] = client
+        self.player_by_client[client] = player
+
+    def read_player_message(self, player, message_class, timeout=MOVE_TIMEOUT):
+        timeout = max(timeout, MOVE_TIMEOUT)
+        client = self.clients_by_player[player]
+
+        return self._read_message(client, message_class, timeout)
+
+    def send_player_message(self, player, message):
+        client = self.clients_by_player[player]
+        self._send_message(client, message)
+
+    def _read_message(self, client, message_class, timeout=GENERAL_TIMEOUT):
+        group = self.group_by_client[client]
+        player = None
+        if client in self.player_by_client:
+            player = self.player_by_client[client]
+
+        try:
+            client.settimeout(timeout)
+            message = read_message_from_conn(client)
+            if isinstance(message, message_class):
+                return message
+
+            # Handle most cases of client errors
+            self.logger.error("User sent wrong message type ({})! Group: {}, Player: {}".format(client, group, player))
+            raise DisqualifiedError(group, player, "Client set wrong message type!")
+        except socket.timeout as err:
+            self.logger.error("Detected Timeout ({})! Group: {}, Player: {}".format(client, group, player))
+            raise DisqualifiedError(group, player, "Client Timeout!", err)
+        except socket.error as err:
+            self.logger.error("Detected Network Error ({})! Group: {}, Player: {}".format(client, group, player))
+            raise DisqualifiedError(group, player, "Network Error!", err)
+
+    def _send_message(self, client, message):
+        try:
+            message.write_to_conn(client)
+        except socket.error as err:
+            group = self.group_by_client[client]
+            player = None
+            if client in self.player_by_client:
+                player = self.player_by_client[client]
+            self.logger.error("Detected Network Error ({})! Group: {}, Player: {}".format(client, group, player))
+            raise DisqualifiedError(group, player, "Network Error!", err)
+
+
+class BasicServer:
+    pass
 
 
 def read_n_bytes(conn, n_bytes):
