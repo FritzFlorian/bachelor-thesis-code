55496051d3add55ccf1b3804734610adea861fb2
FritzFlorian <flo.fritz@t-online.de>
2018-01-24 21:22:19 +0100

    Change optimizer bock to momentum, adam wont work

--- reinforcement/more_maps/neural_network.py
+++ reinforcement/more_maps/neural_network.py
@@ -115,7 +115,7 @@ class SimpleNeuralNetwork(neural_network.NeuralNetwork):
 
         with tf.variable_scope('Training'):
             # Use a simpler optimizer to avoid issues because of it
-            optimizer = tf.train.AdamOptimizer(0.002)
+            optimizer = tf.train.MomentumOptimizer(0.002, 0.9)
             self.training_op = optimizer.minimize(self.loss)
 
         with tf.variable_scope('Logging'):
