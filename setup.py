f0e8aadd81c385ec66bf8cfa3814c6fdeae80b90
FritzFlorian <flo.fritz@t-online.de>
2018-02-19 00:59:19 +0100

    Clean up requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,21 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    name="ReversiAI",
-    ext_modules=cythonize(["reversi/**/*.pyx", "reinforcement/**/*.pyx"])
+    name='bachelor_thesis_code',
+    version='0.1',
+    description='Code of my bachelor thesis',
+    url='bachelor-thesis-code',
+    author='Fritz Florian',
+    license='MIT',
+    packages=['hometrainer'],
+    install_requires=[
+        'Cython',
+        'matplotlib',
+        'numpy',
+        'pyzmq',
+        'tensorflow',
+        'hometrainer'
+    ],
+    zip_safe=False,
+    ext_modules=cythonize(["reversi/**/*.pyx", "reversialphazero/**/*.pyx"])
 )
