dfad321ec50a8ab084f39e0ee17e5b0852257d7d
FritzFlorian <flo.fritz@t-online.de>
2018-02-19 02:09:01 +0100

    Adjust NN name for experiment

--- reversialphazero/normalize_probs/training_master.py
+++ reversialphazero/normalize_probs/training_master.py
@@ -4,7 +4,7 @@ import os
 
 
 work_dir = os.path.join(os.path.curdir, 'test')
-nn_name = 'reversialphazero.more_maps.neural_network.SimpleNeuralNetwork'
+nn_name = 'reversialphazero.normalize_probs.neural_network.SimpleNeuralNetwork'
 
 command_line_interface = CommandLineInterface('MASTER', training_master=True, training_work_directory=work_dir,
                                               training_maps_directory='./maps', training_master_hostname='127.0.0.1',
@@ -21,7 +21,6 @@ def main():
     command_line_interface.config._batch_size = 200
     command_line_interface.config._c_puct = 1
     command_line_interface.execute()
-
 
 
 if __name__ == '__main__':
